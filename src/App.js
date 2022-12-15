cf4c5678d53da779b194b9c25c6c2cfa253283ca
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-12-15 21:58:42 +0500

    Create a counter and buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {
+  collection,
+  doc,
+  onSnapshot,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import db from './components/Firebase';
 
 function App() {
+  const [val, setVal] = useState();
+  const [id, setID] = useState();
+
+  function apiCall(value) {
+    const docRef = doc(db, 'counters', id);
+    updateDoc(docRef, { count: value });
+  }
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'counters'),
+      where('name', '==', 'MyCounter'),
+    );
+    onSnapshot(q, (snapshot) => {
+      let arr = [];
+      arr = snapshot.docs.map((el) => ({ ...el.data(), id: el.id }));
+      setID(arr[0].id);
+      setVal(arr[0].count);
+    });
+  }, []);
+
+  const resHandler = () => {
+    setVal(0);
+    apiCall(0);
+  };
+
+  const clickHandler = (num) => {
+    setVal(val + num);
+    apiCall(val + num);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Counter</h2>
+      <div className="wrapper">
+        <button type="button" className="decBtn" onClick={() => clickHandler(-1)}>
+          - Decrement
+        </button>
+        {val ? <h1>{val}</h1> : <h1>0</h1>}
+        <button type="button" className="incBtn" onClick={() => clickHandler(1)}>
+          + Increment
+        </button>
+      </div>
+      <div className="wrapper">
+        <button type="button" onClick={() => resHandler()}>
+          RESET
+        </button>
+        <button type="button" onClick={() => window.location.reload()}>
+          RELOAD
+        </button>
+      </div>
     </div>
   );
 }
